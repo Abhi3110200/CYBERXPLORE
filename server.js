0c3153dd36d16acc0968094f5512b3ca8dba6bcf
Abhi3110200 <abhijeetdrv@gmail.com>
2025-07-30 05:00:31 +0530

    update server code

--- server.js
+++ server.js
@@ -45,7 +45,10 @@ const connectDB = async () => {
 
 // Middlewares
 app.use(helmet())
-app.use(cors({ origin: FRONTEND_URL, credentials: true }))
+app.use(cors({
+  origin: '*', // ⚠️ Don't use in production
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
 
